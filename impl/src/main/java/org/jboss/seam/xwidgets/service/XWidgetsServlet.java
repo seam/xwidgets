1d38256b962c7bf8d6adb6d6a64107de46e108a6
Shane Bryzak <sbryzak@gmail.com>
2011-04-11 18:07:16 +1000

    more gradle stuff

---
+++ impl/src/main/java/org/jboss/seam/xwidgets/service/XWidgetsServlet.java
@@ -0,0 +1,44 @@
+package org.jboss.seam.xwidgets.service;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class XWidgetsServlet implements Servlet
+{
+   public void init(ServletConfig config) throws ServletException
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+   public ServletConfig getServletConfig()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public void service(ServletRequest req, ServletResponse res)
+         throws ServletException, IOException
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+   public String getServletInfo()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public void destroy()
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+}
