bd364258c2ef39968548931b88d9cd337c47ccc7
Shane Bryzak <sbryzak@gmail.com>
2011-06-07 10:09:22 +1000

    renamed - should be lower case

--- impl/src/main/resources/org/jboss/seam/xwidgets/remotemethoddataset.js
+++ impl/src/main/resources/org/jboss/seam/xwidgets/remotemethoddataset.js
@@ -5,9 +5,23 @@ org.jboss.seam.xwidgets.RemoteMethodDataSet = function() {
   this.registerProperty("remoteClass");
   this.registerProperty("remoteMethod");
   this.registerProperty("qualifiers");
+  this.bean = null;
+  this.values = null;
 };
 
 org.jboss.seam.xwidgets.RemoteMethodDataSet.prototype = new xw.Widget();
   
 org.jboss.seam.xwidgets.RemoteMethodDataSet.prototype.render = function(container) { /* NOOP */};
   
+org.jboss.seam.xwidgets.RemoteMethodDataSet.prototype.open = function() {
+  // TODO add qualifier support
+  if (this.bean === null) {
+    this.bean = Seam.createBean(this.remoteClass);
+  }
+
+  this.bean[this.remoteMethod](this.callback);
+};
+
+org.jboss.seam.xwidgets.RemoteMethodDataSet.prototype.callback = function(result) {
+  alert("Got results: " + result);
+};
